0126a121b5039d52cd3d589bb1371afe359896ef
Nahuel Oroza <nahuel.oroza@aon.com>
2021-02-14 02:08:22 -0300

    Task 5 finished

--- run_kubernetes.sh
+++ run_kubernetes.sh
@@ -4,15 +4,22 @@
 
 # Step 1:
 # This is your Docker ID/path
-# dockerpath=<>
+dockerpath='nforoza/predictor'
 
 # Step 2
 # Run the Docker Hub container with kubernetes
 
+kubectl run predictor\
+    --generator=run-pod/v1\
+    --image=$dockerpath\
+    --port=80 --labels app=predictor_app
+
 
 # Step 3:
 # List kubernetes pods
+kubectl get pods
 
 # Step 4:
 # Forward the container port to a host
+kubectl port-forward predictor 8000:80
 
