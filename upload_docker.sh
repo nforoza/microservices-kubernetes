a641cd0895e08f2c7d365894420cca5dd1850398
Nahuel Oroza <nahuel.oroza@aon.com>
2021-02-13 23:25:52 -0300

    Task 4 finished

--- upload_docker.sh
+++ upload_docker.sh
@@ -5,12 +5,14 @@
 
 # Step 1:
 # Create dockerpath
-dockerpath=nforoza/predictor
+dockerpath="nforoza/predictor"
 
 # Step 2:  
 # Authenticate & tag
 echo "Docker ID and Image: $dockerpath"
+docker login --username=nforoza --email=nahuel.oroza@gmail.com
+docker tag 5de0218e3a01 nforoza/predictor:latest
 
 # Step 3:
 # Push image to a docker repository
-docker push nforoza/predictor:last
+docker push $dockerpath 
